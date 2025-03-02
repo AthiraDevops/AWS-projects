4019042ea420a44a31aadcd62a5781b0003d1ad9
athira.parakkandy@allianz.com <athira.parakkandy@allianz.com>
2025-03-02 18:21:56 +0530

    added attributes in output file

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -19,7 +19,8 @@ resource "local_file" "ssh_public_key" {
 # Creating the key pair in aws using the SSH key created
 
 resource "aws_key_pair" "ssh_key" {
-  key_name_prefix = "${var.project_name}-${var.environment}"
+  
+  key_name_prefix = "${var.project_name}-${var.environment}-"
   public_key      = local_file.ssh_public_key.content
   tags = {
     "Name" = "${var.project_name}-${var.environment}"
